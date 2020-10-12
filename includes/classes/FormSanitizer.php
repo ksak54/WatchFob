985b85f560841098cd25d64d2d5558434e1a617a
ksak54 <kumarsaksham54@gmail.com>
2020-10-12 18:51:24 +0530

    Added methods for sanitizing each field

--- includes/classes/FormSanitizer.php
+++ includes/classes/FormSanitizer.php
@@ -5,6 +5,22 @@
             $inputText = str_replace(" ", "", $inputText);
             $inputText = strtolower($inputText);
             $inputText = ucfirst($inputText);
+            return $inputText;
         }
+        public static function sanitizeFormUsername($inputText){
+            $inputText = strip_tags($inputText);
+            $inputText = str_replace(" ", "", $inputText);
+            return $inputText;
+        }
+        public static function sanitizeFormPassword($inputText){
+            $inputText = strip_tags($inputText);
+            return $inputText;
+        }
+        public static function sanitizeFormEmail($inputText){
+            $inputText = strip_tags($inputText);
+            $inputText = str_replace(" ", "", $inputText);
+            return $inputText;
+        }
+
     }
 ?>
