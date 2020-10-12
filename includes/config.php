418979119ead5c1475ee2595704368d837b61620
ksak54 <kumarsaksham54@gmail.com>
2020-10-12 19:18:41 +0530

    Connected to Database mysql

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+ob_start(); // turns on output buffering
+session_start();
+
+date_default_timezone_set("Asia/Kolkata");
+
+try{
+    $con = new PDO("mysql:dbname=watchfob;host=localhost", "root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+catch(PDOException $e){
+    echo "Connection Failed: " . $e->getMessage();
+}
+?>
