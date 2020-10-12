29236b5e1775df2bb806e68c6be1277f73fd9d9e
ksak54 <kumarsaksham54@gmail.com>
2020-10-12 18:20:14 +0530

    Added class for handling text field Sanitization

---
+++ includes/classes/FormSanitizer.php
@@ -0,0 +1,10 @@
+<?php   
+    class FormSanitizer {
+        public static function sanitizeFormString($inputText){
+            $inputText = strip_tags($inputText);
+            $inputText = str_replace(" ", "", $inputText);
+            $inputText = strtolower($inputText);
+            $inputText = ucfirst($inputText);
+        }
+    }
+?>
